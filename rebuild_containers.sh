2f6f5a0b5185d774b8214fc34f2144bbaf943fe5
Matt Wood <matthewjameswood21@gmail.com>
2021-08-14 14:31:21 -0700

    Distributedloadtesting (#2)
    * Removing build artifacts.
    
    * Added distributed testing with workers and added load testing info to Readme.
    
    * Removed the unused volumes.

--- rebuild_containers.sh
+++ rebuild_containers.sh
@@ -5,8 +5,9 @@ docker compose -f application_stack.yaml down
 docker rmi local/webapi
 docker rmi local/flyway
 docker rmi local/pytests
-docker rmi local/locust
+docker rmi local/locust_main
+docker rmi local/locust_worker
 
 docker volume prune -f
 
-docker compose -f application_stack.yaml up -d
+docker compose -f application_stack.yaml up -d --scale load_worker=6
